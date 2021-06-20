eb0d6366bf36c704ebc96404458cb32901edffad
hsdid <murawskimat00@gmail.com>
2021-06-20 02:40:50 +0200

    Injecting repositories instead of EntityManager and Add FormErrors

--- src/Controller/Registration/RegisterController.php
+++ src/Controller/Registration/RegisterController.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Registration;
 
 use App\Entity\User;
+use App\Form\FormErrors;
 use App\Form\UserType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,22 +23,30 @@ final class RegisterController extends AbstractController
      */
     private  UserPasswordHasherInterface $passwordHasher;
     /**
-     * @var EntityManagerInterface
+     * @var UserRepository
      */
-    private EntityManagerInterface $entityManager;
+    private UserRepository $userRepository;
+
+    /**
+     * @var FormErrors
+     */
+    private FormErrors $formErrors;
 
     /**
      * RegisterController constructor.
      * @param UserPasswordHasherInterface $hasher
-     * @param EntityManagerInterface $manager
+     * @param UserRepository $userRepository
+     * @param FormErrors $formErrors
      */
     public function __construct(
         UserPasswordHasherInterface $hasher,
-        EntityManagerInterface $manager
+        UserRepository $userRepository,
+        FormErrors $formErrors
     )
     {
         $this->passwordHasher = $hasher;
-        $this->entityManager = $manager;
+        $this->userRepository = $userRepository;
+        $this->formErrors = $formErrors;
     }
     /**
      * @Route("/api/register", methods={"POST"}, name="register")
@@ -49,19 +61,19 @@ final class RegisterController extends AbstractController
         $form->submit(json_decode($request->getContent(),true));
 
         if ($form->isValid()) {
+
             $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
 
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
+            try {
+                $this->userRepository->save($user);
+            } catch (OptimisticLockException | ORMException $e) {
+                return $this->json(['errors' => 'User cant be registered']);
+            }
 
             return $this->json(['user' => $user->getUsername(), 'success' => 'User created']);
         }
 
-        $errors = [];
-        foreach ($form->getErrors(true, true) as $error) {
-            $propertyPath = str_replace('data.', '', $error->getCause()->getPropertyPath());
-            $errors[$propertyPath] = $error->getMessage();
-        }
+        $errors = $this->formErrors->getErrors($form);
 
         return $this->json(['errors' => $errors]);
     }
