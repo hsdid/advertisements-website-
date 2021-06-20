353a6ed8ea08a34426ee99c9b7c4c1f003afca48
hsdid <murawskimat00@gmail.com>
2021-06-20 02:37:04 +0200

    Create product type class

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -1,10 +1,38 @@
 <?php
 
-
 namespace App\Form;
 
+use App\Entity\Category;
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProductType
+class ProductType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('description', TextType::class)
+            ->add('price')
+            ->add('techCondition')
+        ;
+    }
 
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+            'csrf_protection' => false,
+        ]);
+    }
 }
